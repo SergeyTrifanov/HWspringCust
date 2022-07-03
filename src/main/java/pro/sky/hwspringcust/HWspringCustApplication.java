5ee748e03222a68b71d3998b4405fcd65162e8b6
sergey <9258228@gmail.com>
2022-07-03 15:12:53 +0300

    ddd

---
+++ src/main/java/pro/sky/hwspringcust/HWspringCustApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.hwspringcust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HWspringCustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HWspringCustApplication.class, args);
+    }
+
+}
+
